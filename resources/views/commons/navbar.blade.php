41b855d9b7b87864a02eac1c0dbea4e2068d9283
hiro <takehiro.ishio77@gmail.com>
2017-04-28 01:36:45 +0000

    until library

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -8,12 +8,12 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="/">Microposts</a>
+                <a class="navbar-brand" href="/">掲示板</a>
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#">Signup</a></li>
-                    <li><a href="#">Login</a></li>
+                    <li><a href="#">ユーザー登録</a></li>
+                    <li><a href="#">ログイン</a></li>
                 </ul>
             </div>
         </div>
